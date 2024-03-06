0b4844791983d384bf54090142f207ef837d4585
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-06 22:58:03 +0530

    Update API URL in getSimplePrice function

--- actions/getSimplePrice.ts
+++ actions/getSimplePrice.ts
@@ -11,7 +11,7 @@ export async function GetSimplePrice({
 }) {
   try {
     const response = await fetch(
-      `${process.env.api_url}?x_cg_demo_api_key=${process.env.api_key}&ids=${ids}&vs_currencies=${vs_currencies}&include_24hr_change=${include_24hr_change}`
+      `${process.env.api_url}${process.env.simple_price_path}?x_cg_demo_api_key=${process.env.api_key}&ids=${ids}&vs_currencies=${vs_currencies}&include_24hr_change=${include_24hr_change}`
     );
     const data = await response.json();
     return data;
