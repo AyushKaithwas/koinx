bc9c517d984f0e585c77a50cacbc95032df22690
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-06 17:23:49 +0530

    Add SentimentCard component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import { MainLayout } from "@/components/containers/mainLayout";
 import TradingViewWidget from "@/components/internal/TradingViewWidget";
 import { Menu } from "@/components/internal/menu";
 import { PerformanceCard } from "@/components/internal/performance-card";
+import { SentimentCard } from "@/components/internal/sentiment-card";
 
 import Image from "next/image";
 
 export default function Home() {
   return (
     <MainLayout className="">
-      <div className="flex flex-col md:w-[66%] h-full rounded-3xl gap-5 overflow-hidden">
+      <div className="flex flex-col md:w-[66%] h-full rounded-3xl gap-5">
         <div className="flex flex-col  w-full h-[45rem]">
           <TradingViewWidget />
         </div>
         <Menu />
         <PerformanceCard />
+        <SentimentCard />
       </div>
     </MainLayout>
   );
